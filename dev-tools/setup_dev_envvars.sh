513c78403b5c02a2dd0558c073a491b79b736d6b
蘇智昊 <nikob@live.cn>
2024-01-04 11:44:14 +0800

    Add scripts for setting env vars for using yarn

---
+++ dev-tools/setup_dev_envvars.sh
@@ -0,0 +1,3 @@
+export sass_binary_site=https://npmmirror.com/mirrors/node-sass/
+export electron_mirror=https://npm.taobao.org/mirrors/electron/
+export electron_builder_binaries_mirror=https://registry.npmmirror.com/-/binary/electron-builder-binaries/
