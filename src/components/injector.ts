a3b86ec89ca93bce347bd290a61edc4a34e0e32b
chentao-123 <1571295310@qq.com>
2024-01-22 09:44:12 +0800

    全局组件注册警告修复

--- src/components/injector.ts
+++ src/components/injector.ts
@@ -1,7 +1,7 @@
-import { App } from "vue";
+import { App, defineAsyncComponent } from "vue";
 export const initGlobalComp = (vm: App) => {
-	const requireComponent = import.meta.glob("@front/components/base/*.vue");
-	for (let fileName in requireComponent) {
-		vm.component(fileName, requireComponent[fileName]);
+	const requireComponent = import.meta.glob("@front/components/base/**/*.vue");
+	for (const [path, component] of Object.entries(requireComponent)) {
+		vm.component(path.substring(path.lastIndexOf("/") + 1, path.lastIndexOf(".")), defineAsyncComponent(component));
 	}
 };
